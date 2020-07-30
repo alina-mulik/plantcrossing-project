c7aa4140b97ad0edf8ae6bbdb85b9be0f67b91b1
alina-mulik <alinamulik96@gmail.com>
2020-07-30 12:08:45 +0300

    Procfile changed to start server from srv.py

--- srv.py
+++ srv.py
@@ -1,6 +1,11 @@
-import django as x
-x =x.__version__
-print(f'django version:{x} - type: {type(x)}')
-
-def fff(y): #имя=fff()
-    print(y)
+import os
+import socketserver
+from http.server import SimpleHTTPRequestHandler
+PORT = int(os.getenv("PORT", 8000))
+print(PORT)
+class MyHandler(SimpleHTTPRequestHandler):
+    pass
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+        print("it" + " works")
+        httpd.serve_forever(poll_interval=1)
