96d0a57faf2f57460df90a31e5b2fc6a531b794e
alina-mulik <alinamulik96@gmail.com>
2020-07-31 20:10:52 +0300

    changed srv file

--- srv.py
+++ srv.py
@@ -4,7 +4,23 @@ from http.server import SimpleHTTPRequestHandler
 PORT = int(os.getenv("PORT", 8000))
 print(PORT)
 class MyHandler(SimpleHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        content = """
+        <!DOCTYPE HTML>
+        <html>
+        <head>
+        <title> </title>
+        </head>
+        <body><strong>Hello Stranger! How are you?</strong></body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header ("Content-length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content.encode())
+
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
         print("it" + " works")
