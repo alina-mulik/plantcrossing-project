9ddd81138af4da7cf7b05f76c6363bab6da45031
alina-mulik <alinamulik96@gmail.com>
2020-08-04 22:41:42 +0300

    trying ti fix styles

--- settings.py
+++ settings.py
@@ -4,3 +4,15 @@ PORT = int(os.getenv("PORT", 8000))
 print(PORT)
 
 CACHE_AGE = 60 * 60 * 24
+
+STATIC_URL = '/static/', '/font-awesome/', '/pict/'
+
+STATICFILES_DIRS = [
+    "static/style.css",
+    "static/login.css",
+    "pict/olive.jpg",
+    "pict/favicon.ico",
+    "pict/cactus.jpg",
+    "font-awesome/css/font-awesome.css",
+    "font-awesome.min.css",
+]
